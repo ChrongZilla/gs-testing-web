a16c6374c08ef4dd99b4b4d2005395277d33da35
ChrongZilla <rudals1475@gmail.com>
2026-08-23 18:32:33 +0900

    test: 애플리케이션 컨텍스트 로딩 sanity check 테스트 추가

--- initial/src/test/java/com/example/testingweb/TestingWebApplicationTests.java
+++ initial/src/test/java/com/example/testingweb/TestingWebApplicationTests.java
@@ -2,13 +2,30 @@ package com.example.testingweb;
 
 import org.junit.jupiter.api.Test;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureRestTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.client.RestTestClient;
 
 @SpringBootTest
+@AutoConfigureRestTestClient
 class TestingWebApplicationTests {
 
+	@Autowired
+	private RestTestClient restTestClient;
+
+	@Test
+	void greetingShouldReturnDefaultMessage() {
+		restTestClient.get().uri("/")
+				.exchange()
+				.expectBody(String.class)
+				.isEqualTo("Hello, World!");
+	}
+	/*
 	@Test
 	void contextLoads() {
 	}
+
+	 */
 
 }
